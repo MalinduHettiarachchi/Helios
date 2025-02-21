4bf9aded8580ab5779387892a14705ce314868ef
Malindu Hettiarachchi <mhssc20@gmail.com>
2025-02-22 02:59:50 +0530

    Develop about us

--- src/component/Deploy.js
+++ src/component/Deploy.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Home from "../component/Home"
 import Navbar from './Navbar'
 import Aboutus from './Aboutus'
+import BackImage from './backimage'
 
 function Deploy() {
   return (
     <div>
-      <Home/>
-      <Navbar/>
-      <Aboutus />
+      <BackImage />
     </div>
   )
 }
